554c3b6fdd352772d556dcbb3b444f24ba35a329
tdaug6 <tayloredaugherty@gmail.com>
2023-02-26 19:37:26 -0500

    better comments on customClasses

--- customClasses.py
+++ customClasses.py
@@ -2,10 +2,18 @@
 from ev3dev2.wheel import *
 from ev3dev2.motor import *
 
+"""
+This class adds a custom wheel size that matches the size of the wheel on our APR.
+The diameter is 56mm and the width is 28mm.
+"""
 class EV3CustomWheel(Wheel):
     def __init__(self):
         Wheel.__init__(self,56,28)
 
+"""
+The custom classes for A and D initialize the port to either A or D.
+This makes the rest of the code cleaner.
+"""
 class CustomMotorA(Motor):
     def __init__(self):
         Motor.__init__(self,OUTPUT_A)
