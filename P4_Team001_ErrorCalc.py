54773d34fbb8e05a02db31fe5a8fc739a2ed1985
Drayxen <xanbowman1@gmail.com>
2023-03-09 21:09:24 -0500

    Upgraded the Error Calc

--- P4_Team001_ErrorCalc.py
+++ P4_Team001_ErrorCalc.py
@@ -1,3 +1,9 @@
+import os
+
+#Clearing the terminal of clutter
+clear = lambda: os.system('cls')
+clear()
+
 #Run into output terminal
 StrErrMeanY = .003889 #cm/inch (ahead)
 StrErrMeanX = .01285 #cm/inch (right)
@@ -5,34 +11,539 @@ StrErrMeanX = .01285 #cm/inch (right)
 TurnErrMeanY = .01797 #cm/inch (ahead)
 TurnErrMeanX = .02023 #cm/inch (right)
 
+xdistance = 0 #in
+ydistance = 0 #in
+
 valid = False
-TurnStr = ""
-Turn = False
 
-while (valid == False):
-    TurnStr = (input("Is the robot going to turn? (y/n): "))
-    if ((TurnStr == "y") or (TurnStr == "Y")):
-        valid = True
-        Turn = True
-    elif ((TurnStr == "n") or (TurnStr == "N")):
-        valid = True
-        Turn = False
+location = input("Please enter the box location (Ex: A2_11): ")
+size = len(location)
+
+# Note: distance from center of robot rotation to edge is 4.75 inches
+# meaning it should move 1 inch closer to the box to determine
+# which box it's going for. For shelf sides facing the start point,
+# add 1 inch; for shelf sides facing away, remove 1 inch.
+
+#Shelf A
+if ((location[0] == 'A') or (location[0] == 'a')):
+    if (location [1] == '1'):
+        if (size == 4):
+            if (location[3] == '1'):
+                ydistance = ydistance + 12 + 1
+                xdistance = xdistance + 6 + 3
+                valid = True
+
+            elif (location[3] == '2'):
+                ydistance = ydistance + 12 + 1
+                xdistance = xdistance + 6 + 9
+                valid = True
+
+            elif (location[3] == '3'):
+                ydistance = ydistance + 12 + 1
+                xdistance = xdistance + 6 + 15
+                valid = True
+
+            elif (location[3] == '4'):
+                ydistance = ydistance + 12 + 1
+                xdistance = xdistance + 6 + 21
+                valid = True
+
+            elif (location[3] == '5'):
+                ydistance = ydistance + 12 + 1
+                xdistance = xdistance + 6 + 27
+                valid = True
+
+            elif (location[3] == '6'):
+                ydistance = ydistance + 12 + 1
+                xdistance = xdistance + 6 + 33
+                valid = True
+
+            elif (location[3] == '7'):
+                ydistance = ydistance + 36 - 1
+                xdistance = xdistance + 6 + 3
+                valid = True
+
+            elif (location[3] == '8'):
+                ydistance = ydistance + 36 - 1
+                xdistance = xdistance + 6 + 9
+                valid = True
+
+            elif (location[3] == '9'):
+                ydistance = ydistance + 36 - 1
+                xdistance = xdistance + 6 + 15
+                valid = True
+
+        elif (size == 5):
+            if (location[4] == '0'):
+                ydistance = ydistance + 36 - 1
+                xdistance = xdistance + 6 + 21
+                valid = True
+
+            elif (location[4] == '1'):
+                ydistance = ydistance + 36 - 1
+                xdistance = xdistance + 6 + 27
+                valid = True
+
+            elif (location[4] == '2'):
+                ydistance = ydistance + 36 - 1
+                xdistance = xdistance + 6 + 33
+                valid = True
+    
+    if (location [1] == '2'):
+        if (size == 4):
+            if (location[3] == '1'):
+                ydistance = ydistance + 36 + 1
+                xdistance = xdistance + 6 + 3
+                valid = True
+
+            elif (location[3] == '2'):
+                ydistance = ydistance + 36 + 1
+                xdistance = xdistance + 6 + 9
+                valid = True
+
+            elif (location[3] == '3'):
+                ydistance = ydistance + 36 + 1
+                xdistance = xdistance + 6 + 15
+                valid = True
+
+            elif (location[3] == '4'):
+                ydistance = ydistance + 36 + 1
+                xdistance = xdistance + 6 + 21
+                valid = True
+
+            elif (location[3] == '5'):
+                ydistance = ydistance + 36 + 1
+                xdistance = xdistance + 6 + 27
+                valid = True
+
+            elif (location[3] == '6'):
+                ydistance = ydistance + 36 + 1
+                xdistance = xdistance + 6 + 33
+                valid = True
+
+            elif (location[3] == '7'):
+                ydistance = ydistance + 60 - 1
+                xdistance = xdistance + 6 + 3
+                valid = True
+
+            elif (location[3] == '8'):
+                ydistance = ydistance + 60 - 1
+                xdistance = xdistance + 6 + 9
+                valid = True
+
+            elif (location[3] == '9'):
+                ydistance = ydistance + 60 - 1
+                xdistance = xdistance + 6 + 15
+                valid = True
+
+        elif (size == 5):
+            if (location[4] == '0'):
+                ydistance = ydistance + 60 - 1
+                xdistance = xdistance + 6 + 21
+                valid = True
+
+            elif (location[4] == '1'):
+                ydistance = ydistance + 60 - 1
+                xdistance = xdistance + 6 + 27
+                valid = True
+
+            elif (location[4] == '2'):
+                ydistance = ydistance + 60 - 1
+                xdistance = xdistance + 6 + 33
+                valid = True
 
 
-distance = 0
-while not (distance > 0):
-    distance = float(input("Please enter the distance (in): "))
-    if (distance <= 0):
-        print("Please enter a positive value.")
+#Shelf B ###########################################
+elif ((location[0] == 'B') or (location[0] == 'b')):
+    if (location [1] == '1'):
+        if (size == 4):
+            if (location[3] == '1'):
+                ydistance = ydistance + 12 + 1
+                xdistance = xdistance + 66 + 3
+                valid = True
 
-XError = 0
-YError = 0
+            elif (location[3] == '2'):
+                ydistance = ydistance + 12 + 1
+                xdistance = xdistance + 66 + 9
+                valid = True
 
-if (Turn):
-    YError = distance * TurnErrMeanY
-    XError = distance * TurnErrMeanX
+            elif (location[3] == '3'):
+                ydistance = ydistance + 12 + 1
+                xdistance = xdistance + 66 + 15
+                valid = True
+
+            elif (location[3] == '4'):
+                ydistance = ydistance + 12 + 1
+                xdistance = xdistance + 66 + 21
+                valid = True
+
+            elif (location[3] == '5'):
+                ydistance = ydistance + 12 + 1
+                xdistance = xdistance + 66 + 27
+                valid = True
+
+            elif (location[3] == '6'):
+                ydistance = ydistance + 12 + 1
+                xdistance = xdistance + 66 + 33
+                valid = True
+
+            elif (location[3] == '7'):
+                ydistance = ydistance + 36 - 1
+                xdistance = xdistance + 66 + 3
+                valid = True
+
+            elif (location[3] == '8'):
+                ydistance = ydistance + 36 - 1
+                xdistance = xdistance + 66 + 9
+                valid = True
+
+            elif (location[3] == '9'):
+                ydistance = ydistance + 36 - 1
+                xdistance = xdistance + 66 + 15
+                valid = True
+
+        elif (size == 5):
+            if (location[4] == '0'):
+                ydistance = ydistance + 36 - 1
+                xdistance = xdistance + 66 + 21
+                valid = True
+
+            elif (location[4] == '1'):
+                ydistance = ydistance + 36 - 1
+                xdistance = xdistance + 66 + 27
+                valid = True
+
+            elif (location[4] == '2'):
+                ydistance = ydistance + 36 - 1
+                xdistance = xdistance + 66 + 33
+                valid = True
+                
+    if (location [1] == '2'):
+        if (size == 4):
+            if (location[3] == '1'):
+                ydistance = ydistance + 36 + 1
+                xdistance = xdistance + 66 + 3
+                valid = True
+
+            elif (location[3] == '2'):
+                ydistance = ydistance + 36 + 1
+                xdistance = xdistance + 66 + 9
+                valid = True
+
+            elif (location[3] == '3'):
+                ydistance = ydistance + 36 + 1
+                xdistance = xdistance + 66 + 15
+                valid = True
+
+            elif (location[3] == '4'):
+                ydistance = ydistance + 36 + 1
+                xdistance = xdistance + 66 + 21
+                valid = True
+
+            elif (location[3] == '5'):
+                ydistance = ydistance + 36 + 1
+                xdistance = xdistance + 66 + 27
+                valid = True
+
+            elif (location[3] == '6'):
+                ydistance = ydistance + 36 + 1
+                xdistance = xdistance + 66 + 33
+                valid = True
+
+            elif (location[3] == '7'):
+                ydistance = ydistance + 60 - 1
+                xdistance = xdistance + 66 + 3
+                valid = True
+
+            elif (location[3] == '8'):
+                ydistance = ydistance + 60 - 1
+                xdistance = xdistance + 66 + 9
+                valid = True
+
+            elif (location[3] == '9'):
+                ydistance = ydistance + 60 - 1
+                xdistance = xdistance + 66 + 15
+                valid = True
+
+        elif (size == 5):
+            if (location[4] == '0'):
+                ydistance = ydistance + 60 - 1
+                xdistance = xdistance + 66 + 21
+                valid = True
+
+            elif (location[4] == '1'):
+                ydistance = ydistance + 60 - 1
+                xdistance = xdistance + 66 + 27
+                valid = True
+
+            elif (location[4] == '2'):
+                ydistance = ydistance + 60 - 1
+                xdistance = xdistance + 66 + 33
+                valid = True
+
+#Shelf C #########################################
+if ((location[0] == 'C') or (location[0] == 'c')):
+    if (location [1] == '1'):
+        if (size == 4):
+            if (location[3] == '1'):
+                ydistance = ydistance + 60 + 1
+                xdistance = xdistance + 6 + 3
+                valid = True
+
+            elif (location[3] == '2'):
+                ydistance = ydistance + 60 + 1
+                xdistance = xdistance + 6 + 9
+                valid = True
+
+            elif (location[3] == '3'):
+                ydistance = ydistance + 60 + 1
+                xdistance = xdistance + 6 + 15
+                valid = True
+
+            elif (location[3] == '4'):
+                ydistance = ydistance + 60 + 1
+                xdistance = xdistance + 6 + 21
+                valid = True
+
+            elif (location[3] == '5'):
+                ydistance = ydistance + 60 + 1
+                xdistance = xdistance + 6 + 27
+                valid = True
+
+            elif (location[3] == '6'):
+                ydistance = ydistance + 60 + 1
+                xdistance = xdistance + 6 + 33
+                valid = True
+
+            elif (location[3] == '7'):
+                ydistance = ydistance + 84 - 1
+                xdistance = xdistance + 6 + 3
+                valid = True
+
+            elif (location[3] == '8'):
+                ydistance = ydistance + 84 - 1
+                xdistance = xdistance + 6 + 9
+                valid = True
+
+            elif (location[3] == '9'):
+                ydistance = ydistance + 84 - 1
+                xdistance = xdistance + 6 + 15
+                valid = True
+
+        elif (size == 5):
+            if (location[4] == '0'):
+                ydistance = ydistance + 84 - 1
+                xdistance = xdistance + 6 + 21
+                valid = True
+
+            elif (location[4] == '1'):
+                ydistance = ydistance + 84 - 1
+                xdistance = xdistance + 6 + 27
+                valid = True
+
+            elif (location[4] == '2'):
+                ydistance = ydistance + 84 - 1
+                xdistance = xdistance + 6 + 33
+                valid = True
+
+    if (location [1] == '2'):
+        if (size == 4):
+            if (location[3] == '1'):
+                ydistance = ydistance + 84 + 1
+                xdistance = xdistance + 6 + 3
+                valid = True
+
+            elif (location[3] == '2'):
+                ydistance = ydistance + 84 + 1
+                xdistance = xdistance + 6 + 9
+                valid = True
+
+            elif (location[3] == '3'):
+                ydistance = ydistance + 84 + 1
+                xdistance = xdistance + 6 + 15
+                valid = True
+
+            elif (location[3] == '4'):
+                ydistance = ydistance + 84 + 1
+                xdistance = xdistance + 6 + 21
+                valid = True
+
+            elif (location[3] == '5'):
+                ydistance = ydistance + 84 + 1
+                xdistance = xdistance + 6 + 27
+                valid = True
+
+            elif (location[3] == '6'):
+                ydistance = ydistance + 84 + 1
+                xdistance = xdistance + 6 + 33
+                valid = True
+
+            elif (location[3] == '7'):
+                ydistance = ydistance + 108 - 1
+                xdistance = xdistance + 6 + 3
+                valid = True
+
+            elif (location[3] == '8'):
+                ydistance = ydistance + 108 - 1
+                xdistance = xdistance + 6 + 9
+                valid = True
+
+            elif (location[3] == '9'):
+                ydistance = ydistance + 108 - 1
+                xdistance = xdistance + 6 + 15
+                valid = True
+
+        elif (size == 5):
+            if (location[4] == '0'):
+                ydistance = ydistance + 108 - 1
+                xdistance = xdistance + 6 + 21
+                valid = True
+
+            elif (location[4] == '1'):
+                ydistance = ydistance + 108 - 1
+                xdistance = xdistance + 6 + 27
+                valid = True
+
+            elif (location[4] == '2'):
+                ydistance = ydistance + 108 - 1
+                xdistance = xdistance + 6 + 33
+                valid = True
+
+
+#Shelf D ###########################################
+elif ((location[0] == 'D') or (location[0] == 'd')):
+    if (location [1] == '1'):
+        if (size == 4):
+            if (location[3] == '1'):
+                ydistance = ydistance + 60 + 1
+                xdistance = xdistance + 66 + 3
+                valid = True
+
+            elif (location[3] == '2'):
+                ydistance = ydistance + 60 + 1
+                xdistance = xdistance + 66 + 9
+                valid = True
+
+            elif (location[3] == '3'):
+                ydistance = ydistance + 60 + 1
+                xdistance = xdistance + 66 + 15
+                valid = True
+
+            elif (location[3] == '4'):
+                ydistance = ydistance + 60 + 1
+                xdistance = xdistance + 66 + 21
+                valid = True
+
+            elif (location[3] == '5'):
+                ydistance = ydistance + 60 + 1
+                xdistance = xdistance + 66 + 27
+                valid = True
+
+            elif (location[3] == '6'):
+                ydistance = ydistance + 60 + 1
+                xdistance = xdistance + 66 + 33
+                valid = True
+
+            elif (location[3] == '7'):
+                ydistance = ydistance + 84 - 1
+                xdistance = xdistance + 66 + 3
+                valid = True
+
+            elif (location[3] == '8'):
+                ydistance = ydistance + 84 - 1
+                xdistance = xdistance + 66 + 9
+                valid = True
+
+            elif (location[3] == '9'):
+                ydistance = ydistance + 84 - 1
+                xdistance = xdistance + 66 + 15
+                valid = True
+
+        elif (size == 5):
+            if (location[4] == '0'):
+                ydistance = ydistance + 84 - 1
+                xdistance = xdistance + 66 + 21
+                valid = True
+
+            elif (location[4] == '1'):
+                ydistance = ydistance + 84 - 1
+                xdistance = xdistance + 66 + 27
+                valid = True
+
+            elif (location[4] == '2'):
+                ydistance = ydistance + 84 - 1
+                xdistance = xdistance + 66 + 33
+                valid = True
+                
+    if (location [1] == '2'):
+        if (size == 4):
+            if (location[3] == '1'):
+                ydistance = ydistance + 84 + 1
+                xdistance = xdistance + 66 + 3
+                valid = True
+
+            elif (location[3] == '2'):
+                ydistance = ydistance + 84 + 1
+                xdistance = xdistance + 66 + 9
+                valid = True
+
+            elif (location[3] == '3'):
+                ydistance = ydistance + 84 + 1
+                xdistance = xdistance + 66 + 15
+                valid = True
+
+            elif (location[3] == '4'):
+                ydistance = ydistance + 84 + 1
+                xdistance = xdistance + 66 + 21
+                valid = True
+
+            elif (location[3] == '5'):
+                ydistance = ydistance + 84 + 1
+                xdistance = xdistance + 66 + 27
+                valid = True
+
+            elif (location[3] == '6'):
+                ydistance = ydistance + 84 + 1
+                xdistance = xdistance + 66 + 33
+                valid = True
+
+            elif (location[3] == '7'):
+                ydistance = ydistance + 108 - 1
+                xdistance = xdistance + 66 + 3
+                valid = True
+
+            elif (location[3] == '8'):
+                ydistance = ydistance + 108 - 1
+                xdistance = xdistance + 66 + 9
+                valid = True
+
+            elif (location[3] == '9'):
+                ydistance = ydistance + 108 - 1
+                xdistance = xdistance + 66 + 15
+                valid = True
+
+        elif (size == 5):
+            if (location[4] == '0'):
+                ydistance = ydistance + 108 - 1
+                xdistance = xdistance + 66 + 21
+                valid = True
+
+            elif (location[4] == '1'):
+                ydistance = ydistance + 108 - 1
+                xdistance = xdistance + 66 + 27
+                valid = True
+
+            elif (location[4] == '2'):
+                ydistance = ydistance + 108 - 1
+                xdistance = xdistance + 66 + 33
+                valid = True
+
+
+#Error Calc
+YError = (ydistance * StrErrMeanY) + (xdistance * (0-TurnErrMeanX))
+XError = (xdistance * StrErrMeanX) + (ydistance * TurnErrMeanY)
+
+if (valid == True):
+    print("The error should be (roughly) {0:.2f} cm to the right and {1:.2f} cm to far.".format(XError, YError))
 else:
-    YError = distance * StrErrMeanY
-    XError = distance * StrErrMeanX
-
-print("The robot should be (roughly) {0:.2f} cm to the right of the desired X point, and {1:.2f} cm ahead of the desired Y point.".format(XError,YError))
+    print("Please enter a valid box position next time.")
