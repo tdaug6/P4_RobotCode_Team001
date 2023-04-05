c1e2e91f728262a91cdfc32a9aecfe61df37ada6
Drayxen <xanbowman1@gmail.com>
2023-04-04 20:21:06 -0400

    Co-authored-by: tdaug6 <tdaug6@users.noreply.github.com> Subtask3 Fix

--- SupportingScripts/BarcodeScanner.py
+++ SupportingScripts/BarcodeScanner.py
@@ -6,40 +6,37 @@ import ev3dev2.auto as ev3
 from ev3dev2.sound import Sound
 from ev3dev2.display import *
 import math
-import Movement
 import time
 from os import *
-from PIDControl import *
 
 #signify start
 spkr = Sound()
 spkr.play_note("D4", 0.25, 100, Sound.PLAY_NO_WAIT_FOR_COMPLETE)
-
-#prepare console for display
-console = Console()
-console.set_font(font='Lat15-TerminusBold24x12')
-
-#Create Scanner
-colorscan = ColorSensor(INPUT_1)
-
-#testing how color sensor works
-
-bar1 = 0
-bar2 = 0
-bar3 = 0
-i = 0
-done = 0
-while (done == 0):
-        scanner = colorscan.reflected_light_intensity
-        print("{0:.0f}".format(scanner))
-        console.text_at("", column=1, row=1, reset_console=True, alignment="L")
-        time.sleep(.1)
+def Scanner():
+    #prepare console for display
+    console = Console()
+    console.set_font(font='Lat15-TerminusBold24x12')
         
+    #Create Scanner
+    colorscan = ColorSensor(INPUT_1)
+
+    #testing how color sensor works
+
+    bar1 = 0
+    bar2 = 0
+    bar3 = 0
+    i = 0
+    done = 0
+    while (done == 0):
+        scanner = colorscan.reflected_light_intensity
+        #print("{0:.0f}".format(scanner))
+        #console.text_at("", column=1, row=1, reset_console=True, alignment="L")
+        time.sleep(.1)
+                    
         if (scanner > 3 and scanner < 12):
-            spkr.play_note("D4", 0.25, 100, Sound.PLAY_NO_WAIT_FOR_COMPLETE)
+            spkr.play_note("F4", 0.25, 100, Sound.PLAY_NO_WAIT_FOR_COMPLETE)
             time.sleep(2)
             while (i < 3):
-                moveDistance(.9, 20)
                 scanner = colorscan.reflected_light_intensity
                 if (i==0):
                     bar1 = colorscan.reflected_light_intensity
@@ -55,16 +52,18 @@ while (done == 0):
                 time.sleep(1)
 
 
-if (bar1 >= 20):
-    if (bar2 >= 20):
-        if (bar3 >= 20):
-            code = "A"
+    if (bar1 >= 20):
+        if (bar2 >= 20):
+            if (bar3 >= 20):
+                code = "A"
+            else:
+                code = "D"
         else:
-            code = "D"
+            code = "B"
     else:
-        code = "B"
-else:
-    code = "C"
+        code = "C"
+            
+#    while True:
+#        console.text_at("%0s"%(code), column=1, row=1, reset_console=False, alignment="L")
 
-while True:
-    console.text_at("%0s"%(code), column=1, row=1, reset_console=False, alignment="L")
+    return code
--- SupportingScripts/Subtask3_FD.py
+++ SupportingScripts/Subtask3_FD.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
-from ev3dev2.sensor import *
 from ev3dev2.console import Console
-from ev3dev2.sensor.lego import *
-import ev3dev2.auto as ev3
 from ev3dev2.sound import Sound
-from ev3dev2.wheel import *
 from ev3dev2.display import *
-import math
-import Movement
 import time
 from os import *
-from PIDControl import *
 from BarcodeScanner import *
+from APR_Location import *
+
+spkr = Sound()
+spkr.play_note("D4", 0.25, 100, Sound.PLAY_NO_WAIT_FOR_COMPLETE)
 
 #prepare console for display
 console = Console()
@@ -19,7 +16,7 @@ console.set_font(font='Lat15-TerminusBold24x12')
 
 #--------------INPUT EXPECTED BAR CODE---------------#
 
-expected = ""
+expected = "B"
 
 #----------------------------------------------------#
 
@@ -29,14 +26,21 @@ expected = ""
 #move to such that the barcode is very close to the furthest edge of the box.
 #Too far is better than too short
 
+#myRobot = APR_Location()
+#myRobot.calibrateGyro()
+
 #activate scanner - might have to back up slightly
-scanny = Scanner
-actual = scanny.Scan()
+actual = Scanner()
+
+spkr.play_note("B4", 0.25, 100, Sound.PLAY_NO_WAIT_FOR_COMPLETE)
+
 
 #compare scanner results to input and output
 if (actual == expected):
+    spkr.play_note("F4", 0.25, 100, Sound.PLAY_NO_WAIT_FOR_COMPLETE)
     while True:
         console.text_at("%0s"%("Match!"), column=1, row=1, reset_console=False, alignment="L")
 else:
+    spkr.play_note("E4", 0.25, 100, Sound.PLAY_NO_WAIT_FOR_COMPLETE)
     while True:
         console.text_at("%0s"%("Does Not Match!"), column=1, row=1, reset_console=False, alignment="L")
